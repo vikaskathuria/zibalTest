2a00fe1877a1055e9118be242196d53b924fac47
t9l_vikas <vikas@tech9logy.in>
2021-01-01 23:44:02 +0530

    initial commit

---
+++ App.js
@@ -0,0 +1,33 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React from 'react';
+import {
+  SafeAreaView,
+  StyleSheet,
+  ScrollView,
+  View,
+  Text,
+  StatusBar,
+} from 'react-native';
+
+import {
+  Header,
+  LearnMoreLinks,
+  Colors,
+  DebugInstructions,
+  ReloadInstructions,
+} from 'react-native/Libraries/NewAppScreen';
+import 'react-native-gesture-handler';
+import MainRoute from './src/Navigator';
+const App=()=>  {
+  return <MainRoute/>
+};
+
+
+export default App;
---
+++ src/Navigator/index.js
@@ -0,0 +1,21 @@
+import * as React from 'react';
+import { Button, View, Text } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import Home from '../Home';
+import Details from '../Detail';
+
+const Stack = createStackNavigator();
+
+function MainRoute() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen name="Home" component={Home} />
+        <Stack.Screen name="Details" component={Details} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+export default MainRoute;
