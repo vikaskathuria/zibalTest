22977239d784b61e3f579d8245e5950248321d1f
t9l_vikas <vikas@tech9logy.in>
2021-01-02 00:05:17 +0530

    initial commit4

--- src/Home/index.js
+++ src/Home/index.js
@@ -1,11 +1,41 @@
 import React, { Component } from 'react'
-import { Text, View } from 'react-native'
+import { Text, View,FlatList } from 'react-native'
 
 export default class Home extends Component {
+    constructor(props) {
+        super(props)
+    
+        this.state = {
+             arr:["1","2","3","4"]
+        }
+    }
+    
+    componentDidMount(){
+        let arr=[]
+        for (let i = 0; i < 15; i++) {
+           for (let j = 0; j < 15; j++) {
+               arr.push({id:i})
+           }
+            
+        }
+    }
+
+    renderItem=({item,index})=>{
+         return(
+             <View>
+
+             </View>
+         )
+    }
     render() {
+        const {arr}=this.state
         return (
-            <View>
-                <Text onPress={()=>this.props.navigation.navigate("Details")}> textInComponent </Text>
+            <View style={{flex:1,backgroundColor:"#fff"}}>
+                <FlatList
+                data={arr}
+                renderItem={this.renderItem}
+
+                />
             </View>
         )
     }
